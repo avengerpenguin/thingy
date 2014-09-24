7355a784beb6480652536447e39adeba5e2aed8a
Ross Fenning <ross.fenning@gmail.com>
2014-09-24 20:56:48 +0100

    Added requirements.txt parsing of dependencies for setup.py.

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+from pip.req import parse_requirements
+
+
+install_reqs = [str(r.req) for r in parse_requirements('requirements.txt')]
+test_reqs = [str(r.req) for r in parse_requirements('test-requirements.txt')]
 
 setup(name='thingy',
 		version = '0.0.0',
@@ -14,4 +19,6 @@ setup(name='thingy',
 		  'Operating System :: OS Independent',
 		  'Programming Language :: Python',
 		],
+      install_requires=install_reqs,
+      tests_require=test_reqs,
 	)
