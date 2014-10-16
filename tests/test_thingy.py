ff03c3c12e703c8753387f2fc957469116438df4
Ross Fenning <fennir01@mc-s083545.local>
2014-10-16 09:14:34 +0100

    Added test for description.

--- tests/test_thingy.py
+++ tests/test_thingy.py
@@ -6,6 +6,10 @@ SCHEMA = Namespace(URIRef('http://schema.org/'))
 KEVIN_BACON = URIRef('http://dbpedia.org/resource/Kevin_Bacon')
 
 
+def english(string):
+    return Literal(string, lang='en')
+
+
 @pytest.fixture
 def home_url():
     return 'http://localhost:5000'
@@ -17,12 +21,24 @@ def thingy_home(home_url):
     return hyperspace.jump(home_url)
 
 
-def test_name(thingy_home):
-    graph = thingy_home.queries['lookup'][0].build(
+@pytest.fixture
+def kevin_bacon(thingy_home):
+    return thingy_home.queries['lookup'][0].build(
         {'iri': 'http://dbpedia.org/resource/Kevin_Bacon'}).submit().data
 
-    assert (
-        KEVIN_BACON,
-        SCHEMA.name,
-        Literal('Kevin Bacon', lang='en')
-    ) in graph
+
+def test_name(kevin_bacon):
+    assert (KEVIN_BACON, SCHEMA.name, english('Kevin Bacon')) in kevin_bacon
+
+
+def test_description(kevin_bacon):
+    descriptions = kevin_bacon.objects(KEVIN_BACON, SCHEMA.description)
+
+    english_description = [
+        description for description in descriptions
+        if description.language == 'en'
+    ][0].toPython()
+
+    assert 'Kevin Norwood Bacon' in english_description
+
+
